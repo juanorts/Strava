780f2a845c7cbba2e6d22fb5d770f672089c8453
Alvaro Ruiz <alvaroruiz@opendeusto.es>
2022-11-24 22:53:32 +0100

    Preparación socket server Facebook

---
+++ FacebookLoginService/src/es/deusto/ingenieria/sd/facebook/server/FacebookSocketServer.java
@@ -0,0 +1,34 @@
+package es.deusto.ingenieria.sd.facebook.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
+import es.deusto.ingenieria.sd.facebook.services.FacebookLoginService;
+
+public class FacebookSocketServer {
+	private static int numClients = 0;
+
+	public static void main(String args[]) {
+		if (args.length < 1) {
+			System.err.println(" # Usage: FacebookSocketServer [PORT]");
+			System.exit(1);
+		}
+
+		// args[1] = Server socket port
+		int serverPort = Integer.parseInt(args[0]);
+
+		try (ServerSocket tcpServerSocket = new ServerSocket(serverPort);) {
+			System.out.println(" - FacebookSocketServer: Waiting for connections '"
+					+ tcpServerSocket.getInetAddress().getHostAddress() + ":" + tcpServerSocket.getLocalPort()
+					+ "' ...");
+
+			while (true) {
+				new FacebookLoginService(tcpServerSocket.accept());
+				System.out.println(
+						" - FacebookSocketServer: New client connection accepted. Client number: " + ++numClients);
+			}
+		} catch (IOException e) {
+			System.err.println("# FacebookSocketServer: IO error:" + e.getMessage());
+		}
+	}
+}
